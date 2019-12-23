31890c0d8b6d0db63f7000813299f2c6aecad921
Stas L <stan.lyashenko@gmail.com>
2019-12-24 00:10:43 +0200

    Add multiple users

--- caller.rb
+++ caller.rb
@@ -6,21 +6,24 @@ require_relative 'user'
 
 class Caller
   include Permitions
-  attr_accessor :user
+  attr_accessor :users
+  def initialize
+    @users = []
+  end
 end
 
 caller = Caller.new
-caller.create_user('John Travolta')
-caller.add_role('admin')
-caller.add_role('user')
+user_name = 'John Travolta'
+caller.create_user(user_name)
+caller.add_role_to_user(user_name, 'admin')
+caller.add_role_to_user(user_name, 'user')
 
 permition = 'reboot the server'
 caller.grant_permition_to_role('admin', permition)
-caller.grant_permition_to_role('user', permition)
 caller.check_role_permition_presence(permition)
 
 permition_2 = 'creating files'
-caller.grant_permition_to_user(permition_2)
+caller.grant_permition_to_user(user_name, permition_2)
 caller.check_user_permition_presence(permition_2)
 
 caller.has_role_permitions?
--- permitions.rb
+++ permitions.rb
@@ -2,44 +2,56 @@
 
 module Permitions
   def create_user(_name)
-    self.user = User.new(_name)
-    puts "'#{user.name}' created!"
+    users << User.new(_name)
+    puts "'#{_name}' created!"
   end
 
-  def add_role(_name)
-    user.roles << Role.new(_name)
-    puts "Added '#{_name}' role to '#{user.name}'!"
+  def add_role_to_user(_name, _role)
+    user = users.find { |n| n.name == _name }
+    user.roles << Role.new(_role)
+    puts "Added '#{_role}' role to '#{user.name}'!"
   end
 
-  def grant_permition_to_role(_name, _permition)
-    user.roles.each do |r|
-      if r.name == _name
-        r.permitions << _permition
-        puts "Added '#{_permition}' permition to '#{r.name}'"
+  def grant_permition_to_role(_role, _permition)
+    users.each do |user|
+      user.roles.each do |r|
+        if r.name == _role
+          r.permitions << _permition
+          puts "Added '#{_permition}' permition to '#{_role}'"
+        end
       end
     end
   end
 
   def check_role_permition_presence(_permition)
-    user.roles.each do |r|
-      puts "Permition '#{_permition}' for role, presence: #{r.permitions.include?(_permition)}"
+    users.each do |user|
+      user.roles.each do |r|
+        puts "Permition '#{_permition}' for role, presence: #{r.permitions.include?(_permition)}"
+      end
     end
   end
 
-  def grant_permition_to_user(_permition)
+  def grant_permition_to_user(_name, _permition)
+    user = users.find { |n| n.name == _name }
     user.permitions << _permition
     puts "Added '#{user.permitions.join}' permition to '#{user.name}'"
   end
 
   def check_user_permition_presence(_permition)
-    puts "Permition '#{_permition}' for user, presence: #{user.permitions.include?(_permition)}"
+    users.each do |user|
+      puts "Permition '#{_permition}' for user, presence: #{user.permitions.include?(_permition)}"
+    end
   end
 
   def has_role_permitions?
-    puts "Role permitions: #{user.roles.any?}"
+    users.each do |user|
+      puts "Role permitions: #{user.roles.any?}"
+    end
   end
 
   def has_user_permitions?
-    puts "User permitions: #{user.permitions.any?}"
+    users.each do |user|
+      puts "User permitions: #{user.permitions.any?}"
+    end
   end
 end
